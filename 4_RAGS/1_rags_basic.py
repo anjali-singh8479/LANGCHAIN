d0164a9b0ca82ec34ee374aa27fb4c6c564e16c6
anjali-singh8479 <shyanj05@gmail.com>
2025-05-13 16:46:03 +0530

    getting into the basics of rags- creating chunks for a file, then embeddings then storing the vector

--- 4_RAGS/1_rags_basic.py
+++ 4_RAGS/1_rags_basic.py
@@ -6,6 +6,15 @@
 # database "vector db" is used to store these vectors
 # when user sends the prompt , the "retrival" will reterive chunks with meaning same as prompt and return it to the llm
 import os
+
+from langchain_text_splitters import TextSplitter,CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+# from dotenv import load_dotenv
+# load_dotenv()
 # getting the absolute path of current dir which is rags folder
 
 current_dir=os.path.dirname(os.path.abspath(__file__))
@@ -22,14 +31,49 @@ current_dir=os.path.dirname(os.path.abspath(__file__))
 #creating a new file with write+read permissions
 # with open("stranger_things.txt", 'w+') as fp:
 #     pass
-os.chdir(current_dir)
+# os.chdir(current_dir)
 # os.mkdir("vectors")
 
 #rename the dir
 # os.rename(os.path.join(current_dir,"vectors"),"db")
 
 
-os.chdir(os.path.join(current_dir,"db"))
-with open("chroma_db" ,"w+") as fp:
-   pass
+# os.chdir(os.path.join(current_dir,"db"))
+# with open("chroma_db" ,"w+") as fp:
+#    pass
+
+persistent_path=os.path.join(current_dir,"db","chroma_db")
+file_path=os.path.join(current_dir,"documents","stranger_things.txt")
+
+if not os.path.exists(persistent_path):
+   print("chroma db does not exists")
+
+loader=TextLoader(file_path, encoding="utf-8")
+document=loader.load()
+
+text_spilter=CharacterTextSplitter(chunk_size=1000 ,chunk_overlap=50)
+docs=text_spilter.split_documents(document)
+
+print(f"No of chunks created {len(docs)}")
+# print(f"First chunk---- \n {docs[0]}")
+
+#creating embeddings using openai
+# embeddings = OpenAIEmbeddings(
+# model="text-embedding-3-small",
+# )
+# vectorstore=Chroma.from_documents(docs,embeddings,persistent_path=persistent_path)
+
+#creating embedding using google claud
+embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+vectorstore=FAISS.from_documents(docs,embedding_model)
+vectorstore.save_local(persistent_path)
+
+print("Finsihed with vector db")
+
+
+
+
+
+
+
 
